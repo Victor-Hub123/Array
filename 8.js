9dd91747dede150aca10db46ef0ea2df5c4390f6
Victor Oliveira <victorcriri313@gmail.com>
2025-04-25 10:49:53 -0300

    8

---
+++ 8.js
@@ -0,0 +1,8 @@
+let idades = ["10", "3", "19", "21", "15"];
+let quantidade = idades.length;
+
+for(let i = 0; i < quantidade; i++){
+    if(idades[i] >= 18){
+        console.log(idades[i]);
+    }
+}
